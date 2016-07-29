2e9bf757f325d321fed3de8e7c6cd976f8d87060
Scott Ouellette <scottx611x@gmail.com>
2016-07-29 11:04:44 -0400

    Fix indentation

--- recieve_SNS.py
+++ recieve_SNS.py
@@ -12,29 +12,33 @@ app = Flask(__name__)
 
 # Route that can handle `GET` and `POST` requests and provide the POST-ed
 # SNS messages to the rendered template
+
+
 @app.route('/', methods=['GET', 'POST'])
 def handle_requests():
     global messages
-    
-	if request.method == 'POST':
-        sys.stdout.write( "Message Recieved")
+
+    if request.method == 'POST':
+        sys.stdout.write("Message Recieved")
         request_content = json.loads(request.get_data())
         sys.stdout.write(request_content)
 
-		# Insert newest messages to front of list
+        # Insert newest messages to front of list
         messages.insert(0, request_content["Message"])
         return str(len(messages))
-    
-	if request.method == 'GET':
+
+    if request.method == 'GET':
         return render_template('SNS.html', message_queue=messages)
 
 # Route that will clear all messages
+
+
 @app.route('/clear')
 def clear():
     global messages
-    
-	# Clear all messages and re-render the template
-	messages = []
+
+    # Clear all messages and re-render the template
+    messages = []
     return render_template('SNS.html', message_queue=messages)
 
 # Run the app :)
@@ -44,3 +48,4 @@ if __name__ == '__main__':
         port=PORT,
         threaded=True
     )
+
